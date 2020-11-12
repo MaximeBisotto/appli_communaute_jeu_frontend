8342d0ea7a309fb1b2973ff255b2542cc9bcb24a
Christophe Manrique <christophe.manrique@gmail.com>
2020-11-12 04:59:40 +0100

    page pour les cas ou une page est en cours de construction

---
+++ src/views/Building.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<main>
+    <h1>Cette page est en cours de construction</h1>
+</main>
+
+<style>
+    h1 {
+        font-size: 3em;
+        margin-left:30%;
+        margin-right: 30%;
+        margin-top: 12%;
+    }
+</style>
